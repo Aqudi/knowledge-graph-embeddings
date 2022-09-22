ae500b1cd2ae8f78415cc38b30a7c3eca48571d2
youbin <ybin.ahn@gmail.com>
2022-09-22 18:25:27 +0900

    feat: Implement TransE init and forward

--- models/transE.py
+++ models/transE.py
@@ -1,16 +1,61 @@
 from typing import Dict, Iterable
 import torch
 from torch import nn
+import numpy as np
 
 from utils.types import TRIPLE
 
 
 class TransE(nn.Module):
-    def __init__(self, embedding_dim, margin, entity2id={}, relation2id={}):
+    def __init__(self, embedding_dim, margin, norm=1, device=None, entity2id={}, relation2id={}):
         super().__init__()
 
-    def forward(self, positive_tripls: Iterable[TRIPLE], negative_triples: Iterable[TRIPLE]):
-        pass
+        # parameters
+        self.embedding_dim=embedding_dim
+        self.margin = margin
+        self.norm = norm
+        self.device = device
+        self.entity2id=entity2id
+        self.relation2id =relation2id
+
+        # training setup
+        self.criterior = nn.MarginRankingLoss(margin=margin)
+        
+        # layers
+        self.relation_embedding = nn.Embedding(num_embeddings=len(self.relation2id), embedding_dim=embedding_dim, padding_idx=0)
+        self.entity_embedding = nn.Embedding(num_embeddings=len(self.entity2id),embedding_dim=embedding_dim, padding_idx=0)
+
+        # initialize embeddings
+        initializer_factor = np.divide(6, np.sqrt(embedding_dim))
+        self.entity_embedding.weight.data.uniform_(-initializer_factor, initializer_factor)
+        self.relation_embedding.weight.data.uniform_(-initializer_factor, initializer_factor)
+
+        # normalize rel_embedding
+        self.relation_embedding.weight.data /= len(self.relation2id)
+
+    def forward(self, positive_triples: Iterable[torch.Tensor], negative_triples: Iterable[torch.Tensor]):
+        positive_distance = self._distance(positive_triples)
+        negative_distance = self._distance(negative_triples)
+        target = torch.tensor([-1], device=self.device)
+        loss = self.criterior(positive_distance, negative_distance, target)
+        return loss
+
+    def normalize_entity_embedding(self):
+        self.entity_embedding.weight.data /= len(self.entity2id)
+
+    def _distance(self, triples:Iterable[torch.Tensor]):
+        head, relation, tail = triples
+        
+        # embedding layer
+        head_embedding = self.entity_embedding(head)
+        relation_embedding = self.relation_embedding(relation)
+        tail_embedding = self.entity_embedding(tail)
+        
+        # calculate norm
+        result = (head_embedding+relation_embedding) - tail_embedding
+        norm = result.norm(p=self.norm, dim=1)
+        print(norm)
+        return norm
 
     @staticmethod
     def create_negative_samples(triples:Iterable[TRIPLE], entity2id:Dict[str, int], device:str=None):
